c4cb261ab479190a6d17e24435c1567d628bb81a
LuPinheiro <gerallupinheiro@gmail.com>
2023-03-01 17:51:19 +0100

    [ADD] sale_project_name_partner

---
+++ sale_project_name_partner/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Project Name Partner",
+    "summary": """
+            Sale Project Name Partner
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/solvosci/slv-project",
+    "depends": [
+        "sale_project"],
+    'installable': True,
+}
---
+++ sale_project_name_partner/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Add commercial partner_id field in the project name in view kanban.
