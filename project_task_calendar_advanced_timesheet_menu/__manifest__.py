d313e864871930648144215eb75dee655875c83e
David Alonso // Solvos <david.alonso@solvos.es>
2023-03-27 18:14:27 +0200

    [ADD] project_task_calendar_advanced_timesheet_menu
    Adds an extra menu with advanced task calendar (provided by
    ``project_task_calendar_advanced`` addon) to Timesheets app.
    
    This addon uses To *Review* main menu added by OCA's ``hr_timesheet_sheet`` to
    achieve this.
    
    This addon can be useful if it's also used ``project_user_group_restriction``,
    that removes *Project* main menu to normal Project users.

---
+++ project_task_calendar_advanced_timesheet_menu/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Project Task - Calendar Advanced Timesheet Menu",
+    "summary": """
+        Adds an extra menu with advanced task calendar (provided by 
+        project_task_calendar_advanced addon) to Timesheets app.
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/solvosci/slv-project",
+    "depends": ["project_task_calendar_advanced", "hr_timesheet_sheet"],
+    "data": ["views/project_task_calendar_advanced_timesheet_menu.xml"],
+    'installable': True,
+}
---
+++ project_task_calendar_advanced_timesheet_menu/readme/DESCRIPTION.rst
@@ -0,0 +1,8 @@
+Adds an extra menu with advanced task calendar (provided by 
+``project_task_calendar_advanced`` addon) to Timesheets app.
+
+This addon uses To *Review* main menu added by OCA's ``hr_timesheet_sheet`` to
+achieve this.
+
+This addon can be useful if it's also used ``project_user_group_restriction``,
+that removes *Project* main menu to normal Project users.
