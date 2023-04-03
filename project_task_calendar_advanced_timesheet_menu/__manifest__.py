784b46e66f6bac6188b6754fc7bf5a419f3134fe
David Alonso // Solvos <david.alonso@solvos.es>
2023-04-03 18:30:20 +0200

    [IMP] project_task_calendar_advanced_timesheet_menu: all tasks menu
    This improvement adds standard "Tasks" tree and others view menu to timesheet app

--- project_task_calendar_advanced_timesheet_menu/__manifest__.py
+++ project_task_calendar_advanced_timesheet_menu/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Project",
     "website": "https://github.com/solvosci/slv-project",
     "depends": ["project_task_calendar_advanced", "hr_timesheet_sheet"],
