985f1ab15bdb19caf31b09da3b7311a72dcf0e5a
David Alonso // Solvos <david.alonso@solvos.es>
2023-04-05 17:57:52 +0200

    [ADD] project_task_followers_mgmt_timesheet
    Timesheet extension for project_task_followers_mgmt addon, that enables
    selecting a proyect in timesheets that matches this addon requirements (accessing
    followers project it user is allowed in at least one task).

---
+++ project_task_followers_mgmt_timesheet/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Project Task - follower type management - extension to timesheets",
+    "summary": """
+        Easy management for allow users in a task set as follower mode
+        - Timesheet extension
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/solvosci/slv-project",
+    "depends": ["project_task_followers_mgmt", "hr_timesheet"],
+}
