e642fa3f1716e4082b2813ebf5eb6d13e0cdf732
David Alonso // Solvos <david.alonso@solvos.es>
2023-04-21 17:32:17 +0200

    [ADD] project_task_followers_mgmt_timesheet_sheet
    Timesheet extension for project_task_followers_mgmt_timesheet addon, that
    adds task finished flag from timesheets in sheet page.
    
    This addon depends on OCA's hr_timesheet_sheet addon.

---
+++ project_task_followers_mgmt_timesheet_sheet/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Project Task - follower type management - extension to timesheet sheets",
+    "summary": """
+        Easy management for allow users in a task set as follower mode
+        - Timesheet Sheets extension
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/solvosci/slv-project",
+    "depends": ["project_task_followers_mgmt_timesheet", "hr_timesheet_sheet"],
+    "data": ["views/hr_timesheet_sheet_views.xml"],
+}
---
+++ project_task_followers_mgmt_timesheet_sheet/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+Timesheet extension for ``project_task_followers_mgmt_timesheet`` addon, that 
+adds task finished flag from timesheets in sheet page.
+
+This addon depends on OCA's ``hr_timesheet_sheet`` addon.
