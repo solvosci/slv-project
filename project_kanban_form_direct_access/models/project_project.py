7bd19e0c41d7b4ffd7ff51258bb98948f55e7ab6
Iria Alonso <iria.alonso@solvos.es>
2024-06-11 14:49:10 +0200

    [ADD] project_kanban_form_direct_access

---
+++ project_kanban_form_direct_access/models/project_project.py
@@ -0,0 +1,16 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
+
+    def action_project_project_view_form(self):
+        return{
+            'view_mode':'form',
+            'res_model':'project.project',
+            'type': 'ir.actions.act_window',
+            'res_id': self.id,
+        }
