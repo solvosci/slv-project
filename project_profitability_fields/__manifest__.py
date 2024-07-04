3eae842f36feb031b129e46f8ed2f7a5fd33c9b0
Iria Alonso <iria.alonso@solvos.es>
2024-07-04 10:27:51 +0200

    [FIX] project_profitability_fields: change translation last_close_date

--- project_profitability_fields/__manifest__.py
+++ project_profitability_fields/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "category": "Project",
     "website": "https://github.com/solvosci/slv-project",
     "depends": ["project"],
